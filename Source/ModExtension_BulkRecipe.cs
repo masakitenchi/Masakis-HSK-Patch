07e415f2c0e968c55a72fe4da9d62357d6292a73
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-05-28 10:42:31 +0900

    Bulk Recipe ModExt

---
+++ Source/ModExtension_BulkRecipe.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Reflection.Emit;
+
+namespace Core_SK_Patch;
+
+public class ModExtension_BulkRecipe : DefModExtension
+{
+    public float MaterialMultiplier = 1.0f;
+    public float ProductMultiplier = 1.0f;
+    public float WorkAmountMultiplier = 1.0f;
+}

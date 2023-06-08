5f302dc566a9468ca853e07239e0a6b50ad84b0d
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-06-08 19:21:46 +0900

    Mod settings; better infestation

---
+++ Source/Settings.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Core_SK_Patch;
+
+public class Settings : ModSettings
+{
+    public static float InfestationPreventionRadius = 10f;
+    public static bool BetterInfestation;
+    public static bool EnableBulkRecipe;
+
+    public override void ExposeData()
+    {
+        base.ExposeData();
+        Scribe_Values.Look(ref BetterInfestation, "BetterInfestation");
+        Scribe_Values.Look(ref EnableBulkRecipe, "EnableBulkRecipe", false);
+        Scribe_Values.Look(ref InfestationPreventionRadius, "InfestationPreventionRadius");
+    }
+}

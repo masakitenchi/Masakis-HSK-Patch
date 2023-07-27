1951c9f04014b2b1fc5d899d1fd6f7c62858de60
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-07-27 20:32:23 +0900

    Engine Industries Compat

---
+++ Source/EngineModExtension.cs
@@ -0,0 +1,7 @@
+﻿namespace Core_SK_Patch;
+
+public class EngineModExtension : DefModExtension
+{
+    public float EngineEfficiency = 1f;
+    public float PowerConsumptionMultiplier = 1f;
+}
---
+++ Source/ITab_Engine.cs
@@ -0,0 +1,22 @@
+﻿namespace Core_SK_Patch;
+
+public class ITab_Engine : ITab
+{
+    public CompEngine CompEngine;
+
+
+    protected override void FillTab()
+    {
+        Rect inRect = new Rect(0f, 0f, size.x, size.y).ContractedBy(5f);
+        Widgets.BeginGroup(inRect);
+
+        Widgets.EndGroup();
+    }
+
+    public override void TabTick()
+    {
+        base.TabTick();
+
+    }
+
+}

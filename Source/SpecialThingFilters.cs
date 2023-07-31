29943b8bd8c493dfb02964c725714d2b08f55113
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-08-01 08:58:02 +0900

    Fix null reference in specialthingfilter

--- Source/SpecialThingFilters.cs
+++ Source/SpecialThingFilters.cs
@@ -7,7 +7,7 @@ public class SpecialThingFilterWorker_MealSimple : SpecialThingFilterWorker
     public override bool CanEverMatch(ThingDef def) => def.IsIngestible;
 
     public override bool AlwaysMatches(ThingDef def) => def.thingCategories.Contains(ThingCategoryDefOf.FoodMeals) && def.ingestible?.preferability == FoodPreferability.MealSimple;
-    public override bool Matches(Thing t) => AlwaysMatches(t.def);
+    public override bool Matches(Thing t) => CanEverMatch(t.def) && AlwaysMatches(t.def);
 }
 
 public class SpecialThingFilterWorker_MealFine : SpecialThingFilterWorker
@@ -15,7 +15,7 @@ public class SpecialThingFilterWorker_MealFine : SpecialThingFilterWorker
     public override bool CanEverMatch(ThingDef def) => def.IsIngestible;
 
     public override bool AlwaysMatches(ThingDef def) => def.thingCategories.Contains(ThingCategoryDefOf.FoodMeals) && def.ingestible?.preferability == FoodPreferability.MealFine;
-    public override bool Matches(Thing t) => AlwaysMatches(t.def);
+    public override bool Matches(Thing t) => CanEverMatch(t.def) && AlwaysMatches(t.def);
 }
 
 public class SpecialThingFilterWorker_MealLavish : SpecialThingFilterWorker
@@ -23,7 +23,7 @@ public class SpecialThingFilterWorker_MealLavish : SpecialThingFilterWorker
     public override bool CanEverMatch(ThingDef def) => def.IsIngestible;
 
     public override bool AlwaysMatches(ThingDef def) => def.thingCategories.Contains(ThingCategoryDefOf.FoodMeals) && def.ingestible?.preferability == FoodPreferability.MealLavish;
-    public override bool Matches(Thing t) => AlwaysMatches(t.def);
+    public override bool Matches(Thing t) => CanEverMatch(t.def) && AlwaysMatches(t.def);
 }
 
 public class SpecialThingFilterWorker_PrimeMeat : SpecialThingFilterWorker
@@ -32,7 +32,7 @@ public class SpecialThingFilterWorker_PrimeMeat : SpecialThingFilterWorker
 
     public override bool AlwaysMatches(ThingDef def) => def.race.meatDef.defName == "Meat_Muffalo";
 
-    public override bool Matches(Thing t) => AlwaysMatches(t.def);
+    public override bool Matches(Thing t) => CanEverMatch(t.def) && AlwaysMatches(t.def);
 }
 
 public class SpecialThingFilterWorker_RawMeat : SpecialThingFilterWorker
@@ -41,7 +41,7 @@ public class SpecialThingFilterWorker_RawMeat : SpecialThingFilterWorker
 
     public override bool AlwaysMatches(ThingDef def) => def.race.meatDef.defName == "Meat_Elephant";
 
-    public override bool Matches(Thing t) => AlwaysMatches(t.def);
+    public override bool Matches(Thing t) => CanEverMatch(t.def) && AlwaysMatches(t.def);
 }
 
 /*public class SpecialThingFilterWorker_FishMeat : SpecialThingFilterWorker

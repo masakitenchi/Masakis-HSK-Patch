ddf6263650a2001709f1dfa4dae24e082db6bf09
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-07-31 21:31:46 +0900

    Raw & Prime meat animal filter

--- Source/SpecialThingFilters.cs
+++ Source/SpecialThingFilters.cs
@@ -1,31 +1,54 @@
-﻿namespace Core_SK_Patch;
+﻿using RimWorld;
+
+namespace Core_SK_Patch;
 
 public class SpecialThingFilterWorker_MealSimple : SpecialThingFilterWorker
 {
     public override bool CanEverMatch(ThingDef def) => def.IsIngestible;
 
-    public override bool Matches(Thing t)
-    {
-        return t.def.thingCategories.Contains(ThingCategoryDefOf.FoodMeals) && t.def.ingestible?.preferability == FoodPreferability.MealSimple;
-    }
+    public override bool AlwaysMatches(ThingDef def) => def.thingCategories.Contains(ThingCategoryDefOf.FoodMeals) && def.ingestible?.preferability == FoodPreferability.MealSimple;
+    public override bool Matches(Thing t) => AlwaysMatches(t.def);
 }
 
 public class SpecialThingFilterWorker_MealFine : SpecialThingFilterWorker
 {
     public override bool CanEverMatch(ThingDef def) => def.IsIngestible;
 
-    public override bool Matches(Thing t)
-    {
-        return t.def.thingCategories.Contains(ThingCategoryDefOf.FoodMeals) && t.def.ingestible?.preferability == FoodPreferability.MealFine;
-    }
+    public override bool AlwaysMatches(ThingDef def) => def.thingCategories.Contains(ThingCategoryDefOf.FoodMeals) && def.ingestible?.preferability == FoodPreferability.MealFine;
+    public override bool Matches(Thing t) => AlwaysMatches(t.def);
 }
 
 public class SpecialThingFilterWorker_MealLavish : SpecialThingFilterWorker
 {
     public override bool CanEverMatch(ThingDef def) => def.IsIngestible;
 
-    public override bool Matches(Thing t)
-    {
-        return t.def.thingCategories.Contains(ThingCategoryDefOf.FoodMeals) && t.def.ingestible?.preferability == FoodPreferability.MealLavish;
-    }
+    public override bool AlwaysMatches(ThingDef def) => def.thingCategories.Contains(ThingCategoryDefOf.FoodMeals) && def.ingestible?.preferability == FoodPreferability.MealLavish;
+    public override bool Matches(Thing t) => AlwaysMatches(t.def);
 }
+
+public class SpecialThingFilterWorker_PrimeMeat : SpecialThingFilterWorker
+{
+    public override bool CanEverMatch(ThingDef def) => def.race is not null;
+
+    public override bool AlwaysMatches(ThingDef def) => def.race.meatDef.defName == "Meat_Muffalo";
+
+    public override bool Matches(Thing t) => AlwaysMatches(t.def);
+}
+
+public class SpecialThingFilterWorker_RawMeat : SpecialThingFilterWorker
+{
+    public override bool CanEverMatch(ThingDef def) => def.race is not null;
+
+    public override bool AlwaysMatches(ThingDef def) => def.race.meatDef.defName == "Meat_Elephant";
+
+    public override bool Matches(Thing t) => AlwaysMatches(t.def);
+}
+
+/*public class SpecialThingFilterWorker_FishMeat : SpecialThingFilterWorker
+{
+    public override bool CanEverMatch(ThingDef def) => def.race is not null;
+
+    public override bool AlwaysMatches(ThingDef def) => def.race.meatDef.defName == "Meat_Fish";
+
+    public override bool Matches(Thing t) => AlwaysMatches(t.def);
+}*/

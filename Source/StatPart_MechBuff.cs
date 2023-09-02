b6d11c34ffbcfb9c86ac6288805faa25104adffe
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-09-02 18:28:48 +0900

    Null Reference Fix The dic was adding a (__instance,null) pair when a ResearchProjectDef has no ResearchMods, this commit should fix #6. closes #6.

---
+++ Source/StatPart_MechBuff.cs
@@ -0,0 +1,17 @@
+﻿/*namespace Core_SK_Patch;
+
+public class StatPart_MechBuff: StatPart
+{
+    public bool Applies(Thing t) => t.def.race?.IsMechanoid ?? false;
+
+    public override string ExplanationPart(StatRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void TransformValue(StatRequest req, ref float val)
+    {
+        if (req.HasThing || !Applies(req.Thing)) return;
+        val += 
+    }
+}*/

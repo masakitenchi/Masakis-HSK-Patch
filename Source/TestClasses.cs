84f674cb2bc872c036eb6d402460895966bbd080
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-09-25 17:07:13 +0900

    Bump update New setting that disable death by low hp Others are WIP

--- Source/TestClasses.cs
+++ Source/TestClasses.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using CombatExtended;
+
+namespace Core_SK_Patch;
 #if DEBUG
 
 
@@ -30,3 +32,68 @@ public static class TestPatches
     }
 }
 #endif
+
+/*public static class OutputBuildingTags
+{
+    [HarmonyPatch(typeof(CombatExtended.Building_TurretGunCE), nameof(CombatExtended.Building_TurretGunCE.GetGizmos))]
+    [HarmonyPostfix]
+    public static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> __result, CombatExtended.Building_TurretGunCE __instance)
+    {
+        foreach (var result in __result)
+            yield return result;
+        if (Prefs.DevMode)
+            yield return new Command_Action()
+            {
+                action = () =>
+                {
+                    Log.Message(string.Join("\n", __instance.def.building.buildingTags.Select(x => x)));
+                },
+                defaultLabel = "DEV: output tags",
+            };
+    }
+}*/
+
+[HarmonyPatch]
+[StaticConstructorOnStartup]
+public class ErrorChecker
+{
+    static ErrorChecker()
+    {
+        StringBuilder sb = new StringBuilder();
+#region Viles_Fix
+        sb.AppendLine("Checking plants that's harvestable but don't have harvestedThingDef. Setting these plants' harvestYield to 0.");
+        foreach(var plant in DefDatabase<ThingDef>.AllDefs.Where(x => x.plant is not null && x.plant.Harvestable && x.plant.harvestedThingDef == null))
+        {
+            sb.AppendLine(" - " + plant.defName);
+            plant.plant.harvestYield = 0;
+        }
+        sb.AppendLine();
+        #endregion
+        
+        /*sb.AppendLine("Checking DamageDefs that's probably missing DamageDefExtensionCE.");
+        foreach(var dmg in DefDatabase<DamageDef>.AllDefs.Where(x => x.armorCategory is not null && (x.armorCategory.defName == "Heat" || x.armorCategory.defName == "Electric")))
+        {
+            if (dmg.GetModExtension<DamageDefExtensionCE>() is null || !dmg.GetModExtension<DamageDefExtensionCE>().isAmbientDamage)
+                sb.AppendLine(" - " + dmg.defName + $" ({dmg.modContentPack.Name})");
+        }*/
+
+        Log.Message(sb.ToString());
+    }
+
+    [HarmonyPatch(typeof(ResearchProjectDef),nameof(ResearchProjectDef.ReapplyAllMods))]
+    public static void Postfix()
+    {
+        Log.Message(StackTraceUtility.ExtractStackTrace());
+    }
+}
+
+/*[HarmonyPatch]
+public static class FoodUtilityFix
+{
+    [HarmonyPatch(typeof(Plant), nameof(Plant.HarvestableNow), MethodType.Getter)]
+    [HarmonyPostfix]
+    public static void Postfix(Plant __instance, ref bool __result)
+    {
+        __result = __instance.def.plant.harvestedThingDef is not null && __result;
+    }
+}*/
---
+++ Source/Transition_LowIdlePowerDraw.cs
@@ -0,0 +1,9 @@
+﻿namespace Core_SK_Patch;
+
+// Temp class for replace CompProperties_LowIdleDraw with CompProperties_Power.idlePowerDraw
+// Vanilla only has code like this.PowerTraderComp.PowerOutput = this.Working ? -this.PowerComp.Props.PowerConsumption : -this.PowerComp.Props.idlePowerDraw;
+// But not all Buildings have Working property. So it's more complicated that I expected. Temporarily halted.
+/*internal class Transition_LowIdlePowerDraw
+{
+}
+*/

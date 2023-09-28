b978a39b509b31a8082534df75c92385d20abd4e
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-09-28 21:29:04 +0900

    Repeatable Research Update

--- Source/GameComponent/GameComponent_ResearchManager.cs
+++ Source/GameComponent/GameComponent_ResearchManager.cs
@@ -1,17 +1,43 @@
-﻿namespace Core_SK_Patch;
+﻿using System.Text.RegularExpressions;
 
+namespace Core_SK_Patch;
+
+
+/*
+ * 
+ */
+[HarmonyPatch]
 public class GameComponent_ResearchManager : GameComponent
 {
-    private Dictionary<ResearchProjectDef, int> _repeatCount;
+    //If it ends with +X (where X is a number)
+    private static readonly Regex labelEndReg = new Regex(@"\+\d+");
 
+    private Dictionary<ResearchProjectDef, int> _repeatCount = new();
+
+    public GameComponent_ResearchManager(Game game)
+    { 
+    }
 
     public void Notify_ResearchFinished(ResearchProjectDef def)
     {
-        if (def.GetModExtension<ModExtension_RepeatableResearch>() is null) return;
-        if (_repeatCount.TryGetValue(def, out var count))
+        var ext = def.GetModExtension<ModExtension_RepeatableResearch>();
+        if (ext is null) return;
+        int count;
+        if (_repeatCount.TryGetValue(def, out count))
             ++_repeatCount[def];
         else
-            _repeatCount.TryAdd(def, 1);
+        {
+            count = 1;
+            _repeatCount.TryAdd(def, count);
+        }
+        //Will change the description & label accordingly some day
+        //Somehow not working?
+        /*if (!labelEndReg.Match(def.label).Value.NullOrEmpty())
+            def.label.Replace(labelEndReg.Match(def.label).Value, $"+{count}");
+        else
+            def.label += $" +{count}";*/
+        if(ext.MaxRepeatableCount > count)
+            Find.ResearchManager.progress[def] = 0f;
     }
 
     public override void ExposeData()
@@ -26,4 +52,24 @@ public class GameComponent_ResearchManager : GameComponent
         _repeatCount = new();
     }
 
+
+    //For back-compatiblity
+    public override void FinalizeInit()
+    {
+        base.FinalizeInit();
+        foreach(var def in DefDatabase<ResearchProjectDef>.AllDefs.Where(x => x.GetModExtension<ModExtension_RepeatableResearch>() != null))
+        {
+            if (def.IsFinished && !this._repeatCount.TryGetValue(def, out _))
+            {
+                _repeatCount.Add(def, 1);
+                Find.ResearchManager.progress[def] = 0f;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ResearchManager),nameof(ResearchManager.FinishProject))]
+    public static void Postfix(ResearchProjectDef proj)
+    {
+        Current.Game.GetComponent<GameComponent_ResearchManager>().Notify_ResearchFinished(proj);
+    }
 }

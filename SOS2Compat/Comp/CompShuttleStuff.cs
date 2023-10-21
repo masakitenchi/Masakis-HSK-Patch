67f527a7b77e84a0915edbfdf5abf565e950823c
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-10-21 21:45:04 +0900

    Revert comp replacement SOS2 has some harmony patch that requires the original comp class. Using transpiler to inject stuff code instead

---
+++ SOS2Compat/Comp/CompShuttleStuff.cs
@@ -0,0 +1,12 @@
+﻿namespace Core_SK_Patch;
+
+public class CompShuttleStuff : ThingComp
+{
+    public ThingDef stuff;
+
+    public override void PostExposeData()
+    {
+        base.PostExposeData();
+        Scribe_Defs.Look(ref stuff, "stuff");
+    }
+}

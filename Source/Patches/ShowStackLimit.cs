f48a555aa5dc74ea4c1d1e2284f92587bf6ed7f2
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-11-03 23:40:58 +0900

    Stack limit addon

---
+++ Source/Patches/ShowStackLimit.cs
@@ -0,0 +1,22 @@
+﻿namespace Core_SK_Patch;
+
+[HarmonyPatch]
+public static class ShowStackLimit
+{
+    //private static readonly StatDef stackLimit = DefDatabase<StatDef>.GetNamed("StackLimit");
+    /*public static bool Prepare()
+    {
+        return true;
+    }*/
+
+    [HarmonyPatch(typeof(ThingDef), nameof(ThingDef.SpecialDisplayStats))]
+    [HarmonyPostfix]
+    public static IEnumerable<StatDrawEntry> ShowStackLimit_PassThroughPostfix(IEnumerable<StatDrawEntry> __result, StatRequest req)
+    {
+        //Log.Message($"StatRequest for {req.defInt.defName}");
+        foreach (var entry in __result)
+            yield return entry;
+        if (req.Pawn is null && req.HasThing && req.thingInt.def.stackLimit != 1)
+           yield return new StatDrawEntry(StatCategoryDefOf.BasicsNonPawnImportant, (string)"CSP_StackLimit".Translate(), Convert.ToString(req.thingInt.def.stackLimit), (string)"CSP_StackLimitDesc".Translate(), 500);
+    }
+}

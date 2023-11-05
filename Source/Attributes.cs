7b3710ff3919f80c931f7e21ae7d631aaefdee7f
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-11-05 10:19:13 +0900

    Bump update & code cleaning New Rimworld.Ref no longer provides publicized assemblies, need to manually add them to publicizer

---
+++ Source/Attributes.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Core_SK_Patch
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class HotSwappableAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class TranspilerAuxliliaryAttribute : Attribute
+    {
+        public string TargetClass;
+        public string TargetMethod;
+        public TranspilerAuxliliaryAttribute(string @class, string method) 
+        {
+            TargetClass = @class;
+            TargetMethod = method;
+        }
+    }
+}
--- Source/Patches/BetterInfestation.cs
+++ Source/Patches/BetterInfestation.cs
@@ -13,7 +13,7 @@ public static class BetterInfestation
     {
         if (InHorDistOf == null)
         {
-            Main.sb.AppendLine("Error: IntVec3.InHorDistOf not found.");
+            Log.Error("Error: IntVec3.InHorDistOf not found.");
             return instructions;
         }
         bool found = false;
@@ -33,7 +33,7 @@ public static class BetterInfestation
         }
         if (!found)
         {
-            Main.sb.AppendLine("Error: Cannot Patch DeepDrill Infestation. Please Contat the author.");
+            Log.Error("Error: Cannot Patch DeepDrill Infestation. Please Contat the author.");
             return instructions;
         }
         /*File.WriteAllLines("E:\\Validatorbefore.txt", instructions.Select(x => x.ToString()));

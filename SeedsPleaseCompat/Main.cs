1039e0039952e4be92d8354be8ff8bd2b9fbd4a1
Manifold Paradox <masakitenchi@users.noreply.github.com>
2023-12-11 10:35:00 +0900

    Show sowPrerequisite for seeds

--- SeedsPleaseCompat/Main.cs
+++ SeedsPleaseCompat/Main.cs
@@ -4,21 +4,22 @@ global using SeedsPlease;
 global using RimWorld;
 global using Verse;
 global using Core_SK_Patch;
+using System.Linq;
 
 namespace Core_SK_Patch;
 
 [HarmonyPatch]
 public static class SeedPatch
 {
-    [HarmonyPatch(typeof(ThingDef),nameof(ThingDef.SpecialDisplayStats))]
+    [HarmonyPatch(typeof(ThingDef), nameof(ThingDef.SpecialDisplayStats))]
     [HarmonyPostfix]
     public static IEnumerable<StatDrawEntry> AddSkillRequirement(IEnumerable<StatDrawEntry> results, ThingDef __instance)
     {
-        foreach(var entry in results)
+        foreach (var entry in results)
         {
             yield return entry;
         }
-        if(__instance is SeedDef seed)
+        if (__instance is SeedDef seed)
         {
             yield return new StatDrawEntry(
                 StatCategoryDefOf.BasicsImportant, "LblSkill".Translate(),
@@ -29,6 +30,14 @@ public static class SeedPatch
                 {
                 new Dialog_InfoCard.Hyperlink(seed.plant)
                 });
+            if (!seed.plant.plant.sowResearchPrerequisites.NullOrEmpty())
+                yield return new StatDrawEntry(
+                   StatCategoryDefOf.BasicsImportant, "LblTech".Translate(),
+                    string.Empty,
+                    "LblTechDesc".Translate(),
+                    501,
+                    hyperlinks: seed.plant.plant.sowResearchPrerequisites.Select(x => new Dialog_InfoCard.Hyperlink(x))
+                    );
         }
     }
 }

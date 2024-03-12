108175604ba087c199c6d18ad53462f259a12cc4
Manifold Paradox <masakitenchi@users.noreply.github.com>
2024-03-12 13:25:18 +0900

    Update build.sh
    Will output the configuration before actually exec build commands

--- Source/build.sh
+++ Source/build.sh
@@ -2,20 +2,32 @@
 dirname=$(realpath $(dirname $0))
 
 config="Debug"
-case $1 in
-	-R )
-		config="Release"
-		;;
-	* )
-		config="Debug"
-		;;
-esac
+clean=""
+while (( "$#" )); do
+	case $1 in
+		-R )
+			config="Release"
+			shift
+			;;
+		-C )
+			clean="Clean;Rebuild"
+			shift
+			;;
+		* )
+			shift
+			;;
+	esac
+done
 
+echo "Current Configuration: -p:Configuration=$config $(if [ -n "$clean" ]; then echo "-t:$clean"; fi)"
+echo "Continue? (y/n)"
+read ans
+if [ "$ans" != "y" ]; then
+	echo "Aborted"
+	exit 1
+fi
 
-
-dotnet restore $dirname/Core_SK_Patch.sln
-
-nuget restore $dirname/Core_SK_Patch.sln
-nuget update $dirname/Core_SK_Patch.sln
-
-MSBuild.exe $dirname/Core_SK_Patch.sln "-p:Configuration=$config" "-t:Clean;Rebuild"
+dotnet restore $dirname/Core_SK_Patch.sln && \
+nuget restore $dirname/Core_SK_Patch.sln && \
+nuget update $dirname/Core_SK_Patch.sln && \
+MSBuild.exe $dirname/Core_SK_Patch.sln "-p:Configuration=$config" "$clean"
